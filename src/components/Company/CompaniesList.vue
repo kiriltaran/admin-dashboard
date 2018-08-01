e97b20722f64904653cc55f4eb573f69b32e53e6
kiriltaran <kiriltaran@gmail.com>
2018-08-01 13:01:19 +0300

    add companies list sorting

--- src/components/Company/CompaniesList.vue
+++ src/components/Company/CompaniesList.vue
@@ -19,11 +19,11 @@
       :style="{height: listHeight + 'px'}"
       class="companies-list">
       <li 
-        v-for="(company, id) in companiesList" 
-        :key="id"
-        :class="companyId === id ? 'active' : ''"
+        v-for="company in companiesList" 
+        :key="company.id"
+        :class="companyId === company.id ? 'active' : ''"
         class="list-item"
-        @click="onClickCompany(id)">
+        @click="onClickCompany(company.id)">
         {{ company.name }}
       </li>
     </ul>
@@ -31,6 +31,8 @@
 </template>
 
 <script>
+import sortBy from 'lodash.sortby';
+
 export default {
   props: {
     companies: {
@@ -52,7 +54,7 @@ export default {
   },
   computed: {
     companiesList() {
-      const result = {};
+      const result = [];
       Object.keys(this.companies).forEach(el => {
         if (
           this.companies[el].name
@@ -60,10 +62,10 @@ export default {
             .includes(this.search.toLowerCase()) ||
           this.companies[el].tin.includes(this.search)
         ) {
-          result[el] = this.companies[el];
+          result.push({ ...this.companies[el], id: el });
         }
       });
-      return result;
+      return sortBy(result, 'name');
     },
   },
   mounted() {
