5d28c64e2352d077e252cac1d3d1b254f4aa7365
Dmitry Teslitsky <dmitryteslitsky@gmail.com>
2018-08-09 13:34:27 +0300

    Make top vacancy regular in 7 days

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,13 @@ const admin = require('firebase-admin');
 const secureCompare = require('secure-compare');
 const subDays = require('date-fns/sub_days');
 const getTime = require('date-fns/get_time');
+const parse = require('date-fns/parse');
+const differenceInDays = require('date-fns/difference_in_days');
 
-const TIME_FIELD = 'publishedTime';
+const PUBLISHED_TIME_FIELD = 'publishedTime';
+const TOP_TIME_FIELD = 'topTime';
 const UNPUBLISHED_STATUS = 'unpublished';
+const REGULAR_STATUS = 'regular';
 
 admin.initializeApp();
 
@@ -17,9 +21,22 @@ function getVacanciesListByDays(days) {
   return admin
     .database()
     .ref('vacancies')
-    .orderByChild(TIME_FIELD)
+    .orderByChild(PUBLISHED_TIME_FIELD)
     .endAt(time)
     .once('value');
+}
+
+function getVacanciesListByStatus(status) {
+  return admin
+    .database()
+    .ref('vacancies')
+    .orderByChild('status')
+    .equalTo(status)
+    .once('value');
+}
+
+function isTopVacancyOld(vacancy) {
+  return differenceInDays(parse(vacancy[TOP_TIME_FIELD]), new Date()) > 7;
 }
 
 exports.unpublish = functions.https.onRequest((req, res) => {
@@ -38,6 +55,37 @@ exports.unpublish = functions.https.onRequest((req, res) => {
         .database()
         .ref('vacancies/' + id)
         .update({ status: UNPUBLISHED_STATUS }));
+
+      Promise.all(promises)
+        .then(() => res.send('ok'))
+        .catch(err => res.status(400).send(err));
+
+      return null;
+    })
+    .catch(err => {
+      res.send(err);
+      return null;
+    });
+});
+
+exports.untop = functions.https.onRequest((req, res) => {
+  const key = req.query.key;
+  if (!secureCompare(key, functions.config().cron.key)) {
+    console.log('The key provided in the request does not match');
+    res.status(403).send('Security key does not match.');
+    return null;
+  }
+
+  getVacanciesListByStatus('top')
+    .then(snapshot => {
+      const vacancies = snapshot.toJSON();
+      const oldTopVacancies = vacancies.filter(v => isTopVacancyOld(v));
+      // prettier-ignore
+      const promises = oldTopVacancies.map(id =>
+        admin
+          .database()
+          .ref('vacancies/' + id)
+          .update({ status: REGULAR_STATUS }));
 
       Promise.all(promises)
         .then(() => res.send('ok'))
